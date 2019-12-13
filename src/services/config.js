8b7045905e0b3c758326dd11bf8ab7f9ca40e7ca
Luca Gouveia <38117857+luca-gouveia@users.noreply.github.com>
2019-12-13 05:03:12 -0300

    removendo...

--- src/services/config.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const http = axios.create({
-    baseURL: 'https://api-gateway-nodejs.herokuapp.com/api/'
-})
-
-export const httpLerMaisTarde = axios.create({
-    baseURL: 'https://api-spring-1.herokuapp.com/api/'
-})
