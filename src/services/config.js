7f2c8d73cdabdf558556b93c583cd032ab7c8ed2
Luca Gouveia <38117857+luca-gouveia@users.noreply.github.com>
2019-12-13 04:57:59 -0300

    heroku ok

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const http = axios.create({
-    baseURL: 'http://localhost:8000/api/'
+    baseURL: 'https://api-gateway-nodejs.herokuapp.com/api/'
 })
 
 export const httpLerMaisTarde = axios.create({
-    baseURL: 'http://localhost:8080/api/'
+    baseURL: 'https://api-spring-1.herokuapp.com/api/'
 })
